9feb6167e9043277b7067027a33195b9df8452a5
ChristopheLeonardi <christophe.leonardi1@gmail.com>
2023-08-25 13:05:23 +0200

    last commit

--- backend/api/MLP_prediction.py
+++ backend/api/MLP_prediction.py
@@ -1,27 +1,34 @@
 import pickle
 import pandas as pd 
 import numpy as np
+import joblib
 from django.templatetags.static import static
-from sklearn.preprocessing import MinMaxScaler 
+from sklearn.preprocessing import OneHotEncoder, StandardScaler, MinMaxScaler 
 from sklearn.neural_network import MLPRegressor
 import os
 
-""" 
-data_dict = (Prix year origin firstHand mileage energy gearbox doors ratedHorsePower powerDIN critAir co2 owners) 
-"""
-def MLP_prediction(data_dict):
+def MLP_prediction(data_dict_list):
 
-    # Load Model
-    model_url = "." + static('regressor_model.sav')
-    loaded_model = pickle.load(open(model_url, 'rb'))
+    model_url = "." + static('rf_model.joblib')
+    loaded_model = joblib.load(model_url)
 
-    X = pd.DataFrame(data_dict)
+    scaler_url = "." + static('scalermm.joblib')
+    loaded_scaler = joblib.load(scaler_url)
 
-    # Prediction
-    scaler = MinMaxScaler()
-    scaler.fit(X)
-    X_test = scaler.transform(X)
-    y_predict = loaded_model.predict(X_test)
+    data_dict = data_dict_list[0]
+    input_values = [data_dict['year'], data_dict['origin'], data_dict['firstHand'],
+                    data_dict['mileage'], data_dict['energy'], data_dict['gearbox'],
+                    data_dict['doors'], data_dict['ratedHorsePower'], data_dict['powerDIN'],
+                    data_dict['critAir'], data_dict['co2'], data_dict['owners']]
+
+    # Convert the values to float
+    input_data = np.array(input_values, dtype=float) 
+    input_data = input_data.reshape(1, -1)
+    
+    # Transform the input data using the loaded scaler
+    scaled_input_data = loaded_scaler.transform(input_data)
+    print(scaled_input_data)
+    y_predict = loaded_model.predict(scaled_input_data)
 
     response = {
         "data": data_dict,
